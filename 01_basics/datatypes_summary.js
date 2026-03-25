7dac83fb6c68a3ea69b92662ab62ee3188e5bfe7
Muhammad Rehmat Waleed <170885359+muhammadrehmatwaleed@users.noreply.github.com>
2026-03-25 20:34:15 +0500

    stack and heap memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -35,3 +35,28 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack and Heap memory
+
+// Primitive data types are stored in stack memory and reference data types are stored in heap memory.
+
+// let myName = "Rehmat"
+// let anotherName = myName
+// anotherName = "Waleed"
+
+// console.log(anotherName);
+// console.log(myName);
+
+
+let userOne = {
+    name: "Rehmat",
+    age: 22,
+}
+let userTwo = userOne
+userTwo.name = "Waleed"
+
+console.log(userOne.name);
+console.log(userTwo.name);
