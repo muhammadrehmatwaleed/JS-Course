16a7180ef2c8ba45095d8a5455c50a96b7e3a1b6
Muhammad Rehmat Waleed <170885359+muhammadrehmatwaleed@users.noreply.github.com>
2026-04-03 19:18:28 +0500

    Objects 2 in js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,63 @@
+// const tinderUser = new Object(); // Object constructor syntax
+
+const tinderUser = {}; // Object literal syntax
+
+
+tinderUser.name = 'Rehmat';
+tinderUser.age = 21;
+tinderUser.isLoggedIn = true;
+tinderUser.lastLoginDate = '2024-06-01';
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email: "waleed@example.com",
+    fullname: {
+        userFullName: {
+            firstName: 'Rehmat',
+            lastName: 'Waleed'
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName.firstName)
+
+
+const obj1 = {
+    key1: 'value1',
+    key2: 'value2'
+}
+
+const obj2 = {
+    key3: 'value3',
+    key4: 'value4'
+}
+
+// const mergedObj = Object.assign({}, obj1, obj2) // Merging obj1 and obj2 into a new object
+
+const mergedObj = {...obj1, ...obj2} // Using spread operator to merge obj1 and obj2 into a new object
+
+// console.log(mergedObj)
+
+
+const user = [
+    {
+        userId: 1,
+        firstName: 'Rehmat',
+        lastName: 'Waleed',
+        email: 'rehmat@example.com'
+    },
+    {
+        userId: 2,
+        firstName: 'Ali',
+        lastName: 'Khan',
+        email: 'muhammad@example.com'
+    }
+]
+
+// console.log(user[0].firstName) // Accessing firstName of the first user in the array
+
+console.log(Object.keys(user[0])) // Getting all keys of the first user object
+console.log(Object.values(user[0])) // Getting all values of the first user object
+console.log(Object.entries(user[0])) // Getting all key-value pairs of the first user object as an array of arrays
+console.log(user[0].hasOwnProperty('email')) // Checking if the first user object has the 'email' property
