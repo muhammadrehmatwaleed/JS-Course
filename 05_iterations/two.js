c363c18a54864677173955cb313e5f7a6b5a666c
Muhammad Rehmat Waleed <170885359+muhammadrehmatwaleed@users.noreply.github.com>
2026-04-09 22:33:15 +0500

    do and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`index: ${index}`);
+//     index = index + 2;
+// }
+
+// let myArray = ["flash", "superman", "batman", "spiderman", "ironman"];
+
+// let i = 0;
+// while (i < myArray.length) {
+//     const element = myArray[i];
+//     console.log(element);
+//     i = i + 1;
+// }
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score<=10);
